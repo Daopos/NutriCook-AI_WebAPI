8b031781c703a5b550448eddc4f874b47b650a25
Ivan Adrian Muñoz <ivan.munoz.50309277@gmail.com>
2026-04-06 12:00:48 -0700

    Added Recipe Model

---
+++ Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace NutriCook_AI_WebAPI.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Difficulty { get; set; }
+
+
+    }
+}
